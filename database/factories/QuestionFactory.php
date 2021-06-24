706c06694b4a18702e25dbfbe77c975ad4bf1ee7
Ahmed Yacoub <ahmed.yacoub@outlook.com>
2021-06-24 23:52:56 +0200

    Implemented Question factory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,11 +2,19 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Question;
+use App\Models\Question;
+use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        //
+        'user_id'   =>  function () {
+            return factory(User::class)->create();
+        },
+        'title'     =>  rtrim($faker->sentence(rand(5, 10)), "."),  // Generate a random sentence between 5 and 10 characters
+        'body'      =>  $faker->paragraph(rand(3, 7)),
+        'views'     =>  rand(0, 10),
+        'answers'   =>  rand(0, 10),
+        'votes'     =>  rand(0, 10),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Question;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // Create 10 question records, creating a user each record.
+        factory(Question::class, 10)->create();
     }
 }
